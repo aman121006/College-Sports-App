317fc76e35f179877ba5b292b54a2be843f9b8f5
Aman Kumar <112406616+aman121006@users.noreply.github.com>
2024-09-18 00:06:11 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,71 @@
-buildscript {
-    val kotlin_version by extra("2.0.0-Beta2")
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-        classpath("com.android.tools.build:gradle:8.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.firebase.crashlytics")
+
+}
+
+android {
+    namespace = "com.titdevelopercommunity.titsports"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.titdevelopercommunity.titsports"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 3
+        versionName = "3.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    repositories {
-        mavenCentral()
+    buildFeatures{
+        viewBinding = true
+    }
+
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0-Beta2" apply false
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.google.firebase:firebase-database-ktx:20.3.1")
+    implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("com.firebaseui:firebase-ui-database:8.0.2")
+    implementation ("com.makeramen:roundedimageview:2.3.0")
+    implementation ("com.airbnb.android:lottie:6.2.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.firebase:firebase-crashlytics:18.6.3")
+    implementation("com.google.firebase:firebase-analytics:21.6.1")
+    implementation("com.google.firebase:firebase-database:20.3.1")
+
+
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.14.2")
+
+    implementation ("com.google.android.play:app-update:2.1.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+
+
+
 }
